d7e717a1e17c16a269b39c1882a7cfdb60c9b074
ClarXo <cheonaru@gmail.com>
2022-05-24 19:36:10 +0900

    api crud

--- server/routes/tweetsRouter.js
+++ server/routes/tweetsRouter.js
@@ -1,10 +1,75 @@
-import express, { application } from 'express';
+import express from 'express';
 import 'express-async-errors';
 
-const app = express.Router();
+let tweets = [
+  {
+    id: '1',
+    text: 'test',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://picsum.photos/64/64',
+  },
+  {
+    id: '2',
+    text: 'test2',
+    createdAt: Date.now().toString(),
+    name: 'Clarko',
+    username: 'clarko',
+    url: 'https://picsum.photos/64/64',
+  },
+];
 
-app.use('/', (req, res, next) => {
-  res.send('tweets');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((tweet) => {
+        return tweet.username === username;
+      })
+    : tweets;
+  res.status(200).json(data);
 });
 
-export default app;
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const data = tweets.find((tweet) => tweet.id === id);
+
+  if (data) {
+    return res.status(200).json(data);
+  }
+  return res.status(404).json({ message: `Tweet id ${id} not found` });
+});
+
+router.post('/', (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    name,
+    username,
+    createdAt: Date.now(),
+  };
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    return res.status(200).json(tweet);
+  }
+  return res.status(404).json({ message: `Tweet id ${id} not found` });
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
+
+export default router;
